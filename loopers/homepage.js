aed90634a4c87e2b5a4b9ceefcbeb599273b7ec7
anantkrsingh <anantkrplay@gmail.com>
2025-09-06 17:34:27 +0530

    fix:notification

--- loopers/homepage.js
+++ loopers/homepage.js
@@ -96,22 +96,10 @@ async function refreshLiveMatchData(matchId) {
       }
     }
     
-
-    const oldWickets =
-      matchData.batting_team === matchData.team_a_id
-        ? oldData.data.team_a_score?.wicket ?? 0
-        : oldData.data.team_b_score?.wicket ?? 0;
-
-    const newWickets =
-      matchData.batting_team === matchData.team_a_id
-        ? matchData.team_a_score?.wicket ?? 0
-        : matchData.team_b_score?.wicket ?? 0;
-
-    if (newWickets > oldWickets) {
-      const lw = matchData.lastwicket;
+    if (JSON.stringify(matchData.lastwicket) !== JSON.stringify(oldData.data.lastwicket)) {
       sendNotification({
         title: `${series} - ${matchData.team_a} vs ${matchData.team_b}`,
-        message: `${lw.player} out, ${lw.run} Runs, ${lw.ball} Balls`,
+        message: `${matchData.lastwicket.player} out, ${matchData.lastwicket.run} Runs, ${matchData.lastwicket.ball} Balls`,
       });
     }
 
